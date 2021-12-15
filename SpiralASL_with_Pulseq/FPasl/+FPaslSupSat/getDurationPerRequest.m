102266a2d2504becbe062e49596ff71fad2cc084
gogospin <33335267+gogospin@users.noreply.github.com>
2021-12-15 15:18:20 +0100

    Update getDurationPerRequest.m

--- SpiralASL_with_Pulseq/FPasl/+FPaslSupSat/getDurationPerRequest.m
+++ SpiralASL_with_Pulseq/FPasl/+FPaslSupSat/getDurationPerRequest.m
@@ -2,4 +2,4 @@ function m_lSBBDurationPerRequest_us = getDurationPerRequest(SupSat)
 
 
 m_lSBBDurationPerRequest_us = ...
-    SupSat.sus_GPSSel.RampUpTime +
+    SupSat.sus_GPSSel.RampUpTime + SupSat.sus_RF.Duration + SupSat.sus_GPSSel.RampDownTime + SupSat.sus_GPSpoil.TotalTime;
