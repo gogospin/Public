fbf9e1e436e375c53c7d8c28de03002c3e8f398c
gogospin <33335267+gogospin@users.noreply.github.com>
2021-12-15 15:57:17 +0100

    Add files via upload

---
+++ SpiralASL_with_Pulseq/Execution_onsite_mbsb.m
@@ -0,0 +1,89 @@
+%clear all
+MrProt.sliceGroupList(1).shift = 0; % 5.7 L, 28.6 A, 48.0 H
+MrProt.sliceGroupList(1).thickness = 1.2; %mm
+MrProt.sliceGroupList(1).sliceperslab = 40;
+MrProt.dynamics = 10;%;
+MrProt.flipAngle = 60;
+MrProt.sliceSeries.size = MrProt.sliceGroupList(1).sliceperslab;
+MrProt.sliceGroupList(1).distanceFactor = 50;
+MrProt.spcl.g3.Spiral_Type = 0;
+MrProt.spcl.single_shot_mode = 1;
+MrProt.spcl.accr_kz = 1;
+MrProt.spcl.private.rotate_mode_ax = 'none';% or 'golden_angle'
+MrProt.spcl.private.increm_mode_sl = 'linear';% or 'binomial' or 'arbit'
+MrProt.spcl.g3.RO_samples = 10240*1.2;
+
+MrProt.sliceGroupList(1).shiftflag=1;
+run('I:\pulseq-master\matlab\Spiral\ASL_FAIRQUIPSSII_spiral_mbsb_invivo.m')
+InfSat.ifs_RF.rf.freqOffset = -InfSat.ifs_RF.rf.freqOffset;
+PreSat.pre_RF.rf.freqOffset = -PreSat.pre_RF.rf.freqOffset;
+SupSat.sus_RF.rf.freqOffset = -SupSat.sus_RF.rf.freqOffset;
+CSatFat.csat_RF.rf.freqOffset = -CSatFat.csat_RF.rf.freqOffset;
+run('I:\pulseq-master\matlab\Spiral\ASL_FAIRQUIPSSII_spiral_mbsb_addBlock_invivo.m')
+%seq.write('C:\Users\P70071217\WMWare_share\pulseq\sequences\test_0411\asl_mbsb.seq');
+%% save data
+save_dir = 'Z:\R.Ma\to_test_07_30\';
+RZ = MrProt.spcl.private.increm_mode_sl;
+RXY = MrProt.spcl.private.rotate_mode_ax;
+iPAT = num2str(MrProt.spcl.accr_kz);
+if strcmp(RXY, 'golden_angle')
+    RXY =  'goldang';
+elseif strcmp(RXY, 'none')
+    RXY = 'norot';
+end
+
+namestr = strcat(save_dir,'external_h45_','iPAT',iPAT,'_RZ',RZ,'_RXY',RXY,'.seq');
+seq.write(namestr);
+
+% define name:
+%% save k traj
+
+clear kx_evol ky_evol kz_evol
+for ll = 1:length(spiral.gx_seg)
+    for i = 1:length(spiral.gx_seg(1).shot)
+        counti = 1;
+        for j = 1:length(spiral.gx_seg(1).shot(1).kslice)
+            for k = 1:length(spiral.gx_seg(ll).shot(i).kslice(j).grad)
+                if counti == 1
+                    kx_evol(ll,i,counti) = spiral.gx_seg(ll).shot(i).kslice(j).grad(k);
+                    ky_evol(ll,i,counti) = spiral.gy_seg(ll).shot(i).kslice(j).grad(k);
+                    kz_evol(ll,i,counti) = spiral.gz_seg(ll).shot(i).kslice(j).grad(k);
+                else
+                    kx_evol(ll,i,counti) = kx_evol(ll,i,counti-1)+spiral.gx_seg(ll).shot(i).kslice(j).grad(k);
+                    ky_evol(ll,i,counti) = ky_evol(ll,i,counti-1)+spiral.gy_seg(ll).shot(i).kslice(j).grad(k);
+                    kz_evol(ll,i,counti) = kz_evol(ll,i,counti-1)+spiral.gz_seg(ll).shot(i).kslice(j).grad(k);
+                end
+                counti = counti+1;
+            end
+        end
+    end
+end
+kx_evol = single(kx_evol)/100*2*pi*43.576;
+ky_evol = single(ky_evol)/100*2*pi*43.576;
+kz_evol = single(kz_evol)/100*2*pi*43.576;
+%%
+seg = 1;nn=1;
+figure,
+for i = 1:size(kx_evol,2)
+    scatter3(kx_evol(seg,i,1:end/nn),ky_evol(seg,i,1:end/nn),kz_evol(seg,i,1:end/nn),'b.');
+    hold on;
+end
+namestr = strcat(save_dir,'ktraj_h45_','iPAT',iPAT,'_RZ',RZ,'_RXY',RXY,'.mat');
+save(namestr,'kx_evol','ky_evol','kz_evol');
+%%
+counti=1;
+for i = 1:10240*1.2*2.5/10-2
+    if counti == 1
+        kx_evol(counti) = spiral.GradBuf(1,i);
+        ky_evol(counti) = spiral.GradBuf(2,i);
+        kz_evol(counti) = spiral.caipiblip.Grad(i)*spiral.caipiblip.GradAmp;
+    else
+        kx_evol(counti) = kx_evol(counti-1)+spiral.GradBuf(1,i);
+        ky_evol(counti) = ky_evol(counti-1)+spiral.GradBuf(2,i);
+        kz_evol(counti) = kz_evol(counti-1)+spiral.caipiblip.Grad(i)*spiral.caipiblip.GradAmp;
+    end
+    counti = counti+1;
+end
+kx_evol = single(kx_evol)/100*2*pi*43.576;
+ky_evol = single(ky_evol)/100*2*pi*43.576;
+kz_evol = single(kz_evol)/100*2*pi*43.576;
